0fd38156e03f12093fccad43c32740b6b62ae04e
soldesk-PC\soldesk <chungwa2@nate.com>
2017-05-17 13:20:51 +0900

    정재흠 캘린더 한글화

--- WebContent/html/zabuto_calendar.js
+++ WebContent/html/zabuto_calendar.js
@@ -674,6 +674,13 @@ $.fn.zabuto_calendar_language = function (lang) {
                 dow_labels: ["Ma", "Ti", "Ke", "To", "Pe", "La", "Su"]
             };
             break;
+            
+        case 'kr':
+            return {
+                month_labels: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+                dow_labels: ["월", "화", "수", "목", "금", "토", "일"]
+            };
+            break
     }
 
 };
